5f6826f70e05ae83e49fe80c47eb52f3bec06b60
Mike Hurley <mhurley@hurley-consulting.io>
2018-06-07 16:24:35 -0400

    Console Practice

--- TheAdventureGame/Program.cs
+++ TheAdventureGame/Program.cs
@@ -6,7 +6,12 @@ namespace TheAdventureGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the Adventure Game!");
+            Console.WriteLine("Which way would you like to go? [Left, Right, or Straight Ahead?]");
+
+            var choice = Console.ReadLine();
+
+            Console.WriteLine("You chose " + choice + ".");
             Console.ReadLine();
         }
     }
